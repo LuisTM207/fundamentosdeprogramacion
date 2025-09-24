c0f90abfd25164341f5fc9c7c180407445e31775
LuisTM207 <luis465125@gmail.com>
2025-09-24 07:43:47 -0600

    Create raizcuadrada.py

---
+++ unidad1/raizcuadrada.py
@@ -0,0 +1,12 @@
+<<<<<<< HEAD
+import math
+
+numuero = 16
+raiz = math.sqrt(numero)
+=======
+import math
+
+numuero = 16
+raiz = math.sqrt(numero)
+>>>>>>> 35087400e8c2db1b4ee1189eabf96594344f9320
+print(f"la raiz cuadrada de {numero} es {raiz}")
