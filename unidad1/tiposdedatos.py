f9c5f5c2a26a8b0c500d986d861a0428268b4985
LuisTM207 <luis465125@gmail.com>
2025-09-24 07:43:44 -0600

    Create tiposdedatos.py

---
+++ unidad1/tiposdedatos.py
@@ -0,0 +1,16 @@
+<<<<<<< HEAD
+entero = 10
+flotante = 3.14
+cadena = "Ed Flores"
+booleana = True
+
+print(entero)
+=======
+entero = 10
+flotante = 3.14
+cadena = "Ed Flores"
+booleana = True
+
+print(entero)
+>>>>>>> 9858262447a02e49db1078a7c201d8b77d6892b4
+print(flotante)
