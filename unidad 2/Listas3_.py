ae1804dc16f1a71f188aa82cfb6b0a8203f1628e
LuisTM207 <luis465125@gmail.com>
2025-09-24 07:43:54 -0600

    Create Listas3_.py

---
+++ unidad 2/Listas3_.py
@@ -0,0 +1,26 @@
+lista_Luis = []
+
+print(" lista_Luis ")
+Elemento1 = input(" Agrega nombre: ")
+lista_Luis.append(nombre)
+
+Elemento2 = input("Agrega preparatoria: ")
+lista_Luis.append(preparatoria)
+
+Elemento3 = input("Agrega lugar de residencia: ")
+lista_Luis.append(residencia)
+
+Elemento4 = input("Agrega edad: ")
+lista_Luis.append(edad)
+
+Elemento5 = input("Agrega carrera: ")
+lista_Luis.append(carrera)
+
+
+
+print("\n📌 Tu lista es:")
+for producto in lista:
+    print(f"- {Elemento}")
+    
+print("\n✅ ¡Lista creada con éxito!")
+
