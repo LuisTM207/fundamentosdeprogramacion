8ae65692653a868d72469d5992efc7be39b36b1f
LuisTM207 <luis465125@gmail.com>
2025-09-24 07:43:50 -0600

    Create Listas2_.py

---
+++ unidad 2/Listas2_.py
@@ -0,0 +1,20 @@
+#se crea una lista llamada lista_compras
+lista_compras = []
+
+print("🛒 Lista de Compras 🛒")
+producto1 = input(" Agrega el primer producto: ")
+#agregar producto
+lista_compras.append(producto1)
+
+producto2 = input("Agrega el segundo producto: ")
+lista_compras.append(producto2)
+#agregar producto
+producto3 = input("Agrega el tercer producto: ")
+lista_compras.append(producto3)
+
+print("\n📌 Tu lista de compras es:")
+for producto in lista_compras:
+    print(f"- {producto}")
+    
+print("\n✅ ¡Lista creada con éxito!")
+#https://www.webfx.com/tools/emoji-cheat-sheet/
